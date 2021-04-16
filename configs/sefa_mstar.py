aec36ff1b31465b4f71b21d3d27bc859c710825e
McHz1s <316793523@qq.com>
2021-04-16 17:52:01 +0800

    just some test nothing specail

--- configs/sefa_mstar.py
+++ configs/sefa_mstar.py
@@ -6,19 +6,19 @@ and `num_workers`.
 """
 
 viz_size = 128
-gpus = '5'
+gpus = '6'
 
 work_dir = '/data3/lyz/cache/sefa'
-checkpoint_path = '/data3/lyz/cache/genforce/stylegan_mstar_6z_degree1-2/2021-4-14-16-45-54/checkpoint_iter070000.pth'
+checkpoint_path = '/data3/lyz/cache/genforce/stylegan_mstar_2z_degree1-2/2021-4-15-19-08-18/checkpoint_iter050000.pth'
 gt_data_cfg = dict(root_dir='/data3/lyz/dataset/mstar/TRAINT72_132INF.MAT',
                    degree_interval_list=[[0, 90]])
 
 generator_config = dict(
     gan_type='stylegan',
     resolution=128,
-    z_space_dim=6, w_space_dim=6,
+    z_space_dim=2, w_space_dim=2,
     image_channels=1, final_sigmoid=True
 )
 
-sefa_cfg = dict(num_samples=2, num_semantics=3, start_distance=-7, end_distance=7, step=118, seed_range=[0, 1],
+sefa_cfg = dict(num_samples=4, num_semantics=2, start_distance=-6, end_distance=6, step=118, seed_range=[0, 1, 2, 3],
                 trunc_psi=1.0, trunc_layers=0, layer_idx='all')
